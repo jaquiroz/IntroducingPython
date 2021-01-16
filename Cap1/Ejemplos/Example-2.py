2e2a1cf955be1ab8b568e2b444bf84d13c0f0fe1
jaquiroz <jorge.armando.quiroz.perez@gmail.com>
2021-01-16 15:04:56 -0400

    Inicio cap1

---
+++ Cap1/Ejemplos/Example-2.py
@@ -0,0 +1,10 @@
+#Spells de Harry Potter
+spells = [
+    "Riddikulus!",
+    "Wingardium Leviosa!",
+    "Avada Kedavra!",
+    "Expecto Patronum!",
+    "Nox!",
+    "Lummos!",
+]
+print(spells[3])
---
+++ Cap1/Ejemplos/Example-3.py
@@ -0,0 +1,9 @@
+#Frases de los tres chiflados
+quotes = {
+    "Moe":"Un tipo listo, eh?",
+    "Larry":"Ow!",
+    "Curly":"Nuyk nyuk!",
+}
+
+chiflado = "Larry"
+print(chiflado, " dice:", quotes[chiflado])
